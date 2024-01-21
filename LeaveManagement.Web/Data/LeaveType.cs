e78215b26ad72190ce0ac5212dbe377475338ab6
famgate <nolezquery@outlook.com>
2024-01-21 10:07:00 +0600

    Added Database Tables and Migrations. End Implement Database Section

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveType:BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
